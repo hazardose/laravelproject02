2baff1b80498a27b57395fb84032e611451979f7
Shandy <shandymartabz@gmail.com>
2020-07-19 20:27:48 +0800

    generate question model via artisan

---
+++ app/Question.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = ['title', 'body'];
+
+    public function user() {
+        return $this->belongTo(User::class);
+    }
+    
+    public function questions() 
+    {
+        return $this->hasMany(Question::class);
+
+    }
+
+    public function setTitleAttribute($value);
+    {
+        $this->attribute['title'] = $value;
+        $this->attribute['slug'] = $value;
+        
+    }
+
+}
